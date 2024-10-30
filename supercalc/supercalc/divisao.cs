60a056b658ea1397d9a94e1df417cf0f26ffad54
MIGUEL LONNI DO PRADO <mlonniprado@gmail.com>
2024-10-30 19:42:36 -0300

    Adicionada função de divisão

---
+++ supercalc/supercalc/divisao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supercalc
+{
+    public class divisao
+    {
+        public double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
